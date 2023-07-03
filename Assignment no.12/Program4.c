a79122c76088543c9aa3ea072521145db39c832a
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-07-03 08:13:19 +0530

    Add files via upload

---
+++ Assignment no.12/Program4.c
@@ -0,0 +1,28 @@
+/*
+Write a program which accept number from the user and display below pattern.
+
+Input : 5
+Output : # 1 * # 2 * # 3 * # 4 * # 5 *
+*/
+
+#include <stdio.h>
+
+void pattern(int num)
+{
+    int i;
+
+    for (i = 1; i <= num; i++)
+        printf("# %d * ", i);
+}
+
+int main(void)
+{
+    int num;
+
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    pattern(num);
+
+    return 0;
+}
