36afdc563394e97c0375be4ef71255a4f7aa0b42
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-07-04 08:12:26 +0530

    Add files via upload

---
+++ Assignment no.13/Program2.c
@@ -0,0 +1,40 @@
+/*
+Write a program which accept rows and numbers of column from user and display below pattern.
+
+Input : row = 4, col = 3
+Output :
+1   2   3
+1   2   3 
+1   2   3 
+1   2   3
+
+*/
+
+#include <stdio.h>
+
+void pattern(int row, int col)
+{
+    int i, j;
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 1; j <= col; j++)
+            printf("%d   ", j);
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    int row, col;
+
+    printf("Enter rows : ");
+    scanf("%d", &row);
+
+    printf("Enter columns : ");
+    scanf("%d", &col);
+
+    pattern(row, col);
+
+    return 0;
+}
