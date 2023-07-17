3b9362316d77fc64b438c0005cbbd6df389bff10
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-07-17 09:01:17 +0530

    Add files via upload

---
+++ Assignment no.24/Program1.c
@@ -0,0 +1,37 @@
+/*
+Write a program which accept string from user and count number of capital characters.
+
+Input : "Marvellous Multi OS"
+Output : 4
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int count_capital(char *str)
+{
+    int count = 0;
+
+    while (*str != '\0')
+    {
+        if (*str >= 'A' && *str <= 'Z')
+            count++;
+        str++;
+    }
+    return count;
+}
+
+int main(void)
+{
+    int result;
+    char arr[20];
+
+    printf("Enter the string : ");
+    fgets(arr, sizeof(arr), stdin);
+
+    result = count_capital(arr);
+
+    printf("There are %d characters are capital in given string.\n", result);
+
+    return 0;
+}
