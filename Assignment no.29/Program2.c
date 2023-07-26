bb84e3568e1503ed0728c5af286c5c6148bd3713
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-07-26 11:04:18 +0530

    Add files via upload

---
+++ Assignment no.29/Program2.c
@@ -0,0 +1,35 @@
+/*
+Write a program which checks whether 5th and 18th bit is on or off.
+*/
+
+#include <stdio.h>
+
+int check_bit(unsigned int num)
+{
+    int result = 0;
+    int mask = 0x00020010;
+    
+    result = mask & num;
+
+    if (result == mask)
+        return 1;
+    return 0;
+}
+
+int main(void)
+{
+    unsigned int num;
+    int result;
+
+    printf("Enter number : ");
+    scanf("%d", &num);
+
+    result = check_bit(num);
+
+    if (result == 1)
+        printf("Bit is on\n");
+    else
+        printf("Bit is off\n");
+
+    return 0;
+}
