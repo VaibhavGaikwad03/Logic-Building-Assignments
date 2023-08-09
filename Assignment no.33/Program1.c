82b5805299ce6f9c3fce03b4791964330083ced2
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-08-09 19:31:30 +0530

    Add files via upload

---
+++ Assignment no.33/Program1.c
@@ -0,0 +1,39 @@
+/*
+Write a program which accepts one number from user and count number of ON(1) bits in it without using % and / operator.
+
+Input : 11
+Output : 3
+
+*/
+
+#include <stdio.h>
+
+int count_on_bits(unsigned int num)
+{
+    int count = 0;
+    int mask = 0x00000001;
+
+    while (num != 0)
+    {
+        if (num & mask)
+            count++;
+        num = num >> 1;
+    }
+    return count;
+}
+
+int main(void)
+{
+    int pos;
+    int result;
+    unsigned int num;
+
+    printf("Enter number : ");
+    scanf("%d", &num);
+
+    result = count_on_bits(num);
+
+    printf("Number of on bits are : %d\n", result);
+
+    return 0;
+}
