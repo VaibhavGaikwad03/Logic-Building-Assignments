85cbcef662caadcdf458bc78ecaee26c6c7f27fa
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-08-18 08:10:21 +0530

    Add files via upload

---
+++ Assignment no.40/Program4.c
@@ -0,0 +1,39 @@
+/*
+Write an application which accept file name from user and display size of file.
+
+Input : Demo.txt
+Output : File size is 56 bytes.
+*/
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main(void)
+{
+    int fd;
+    int count;
+    int count_bytes = 0;
+    char buffer[20];
+    char file_name[20];
+
+    printf("Enter file name : ");
+    scanf("%[^'\n']s", file_name);
+
+    fd = open(file_name, O_RDONLY);
+
+    if (fd == -1)
+    {
+        perror("ERROR");
+        exit(1);
+    }
+    
+    while (count = read(fd, buffer, sizeof(buffer)))
+        count_bytes += count;
+
+    printf("File size is %d bytes.\n", count_bytes);
+
+    return 0;
+}
